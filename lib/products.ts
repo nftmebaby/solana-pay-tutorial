0282dcf36a4042dc493971cefbe36150bf1f8eae
nftmebaby <alexjbrand1@gmail.com>
2022-04-07 11:19:20 -0400

    Customization

--- lib/products.ts
+++ lib/products.ts
@@ -1,16 +1,16 @@
 export const products = [
   {
     id: 'box-of-cookies',
-    name: 'Box',
-    description: 'A delicious box of handmade cookies',
+    name: 'Dress',
+    description: 'A handmade dress',
     unitName: 'box', // shows after the price, eg. 0.05 SOL/box
     priceSol: 0.05,
     priceUsd: 5,
   },
   {
     id: 'basket-of-cookies',
-    name: 'Basket',
-    description: 'A large basket of handmade cookies',
+    name: 'Skirt',
+    description: 'A skirt made in India',
     unitName: 'basket',
     priceSol: 0.1,
     priceUsd: 10,
